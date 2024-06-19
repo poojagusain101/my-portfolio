ccc225d91063c477f46bf5101f03974552126def
unknown <poojagusain101@gmail.com>
2024-06-19 11:20:38 +0530

    updated

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 import styles from "./page.module.css";
-import Navbar from "@/components/Navbar/Navbar.js";
-import Hero from "@/components/Hero/Hero.js";
+import Navbar from "@/components/Navbar/Navbar";
+import Hero from "@/components/Hero/Hero";
 import { About } from "@/components/About/About";
 import { Skills } from "@/components/Skills/Skills";
 import Education from "@/components/Education/Education";
